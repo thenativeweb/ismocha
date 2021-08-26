c4e04049f5f6d4e8d5c70fb19aa6347450488cec
Noah Hummel <strangedev@posteo.net>
2021-08-26 11:36:44 +0200

    fix: detect mocha also by the globals it exposes

--- lib/isMocha.ts
+++ lib/isMocha.ts
@@ -1,3 +1,9 @@
+const testInterfaces = [
+  [ 'describe', 'context', 'it', 'specify', 'before', 'after', 'beforeEach', 'afterEach' ],
+  [ 'suite', 'test', 'suiteSetup', 'suiteTeardown', 'setup', 'teardown' ],
+  [ 'suite', 'test', 'before', 'after', 'beforeEach', 'afterEach' ]
+];
+
 const isMocha = function (): boolean {
   // If we run this in the browser, e.g. by using webpack, there is no
   // process.argv array. Anyway, then, we are also not run using Mocha.
@@ -5,7 +11,22 @@ const isMocha = function (): boolean {
     return false;
   }
 
-  return process.argv[1].includes('.bin/mocha');
+  const globals = Object.keys(global);
+  let hasTestInterface = false;
+
+  /* eslint-disable no-labels */
+  nextInterface: for (const testInterface of testInterfaces) {
+    for (const functionName of testInterface) {
+      if (!globals.includes(functionName)) {
+        continue nextInterface;
+      }
+    }
+    hasTestInterface = true;
+    break;
+  }
+  /* eslint-enable no-labels */
+
+  return process.argv[1].includes('.bin/mocha') || hasTestInterface;
 };
 
 export { isMocha };
